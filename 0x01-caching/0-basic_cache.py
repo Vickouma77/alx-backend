4f30c7c4034a05cf6cc69ddf661183d32030be6a
vickouma77 <Vickprestone20@gmail.com>
2023-07-25 12:45:28 +0300

    Basic dictionary

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+""" Basic dictionary: 
+    Create a class BasicCache that inherits from 
+    BaseCaching and is a caching system
+"""
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """
+    BasicCache class:
+    Create a class BasicCache that inherits from
+    BaseCaching and is a caching system
+    """
+
+    def put(self, key, item):
+        """
+        Add an item in the cache
+        """
+        if key and item:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        Get an item by key
+        """
+        if key and key in self.cache_data:
+            return self.cache_data[key]
+        return None
