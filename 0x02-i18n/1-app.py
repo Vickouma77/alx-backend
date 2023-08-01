614911013208f35365fb0d94d658f438aba0e604
vickouma77 <Vickprestone20@gmail.com>
2023-08-01 14:41:01 +0300

    Basic Babel setup

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -3,7 +3,7 @@
 Basic Babel setup
 """
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_babel import Babel
 
 app = Flask(__name__)
@@ -13,3 +13,19 @@ babel = Babel(app)
 app.config['BABEL_DEFAULT_LOCALE'] = 'en'
 app.config['BABEL_DEFAULT_TIMEZONE'] = 'UTC'
 app.config['LANGUAGES'] = ['en', 'fr']
+
+
+@babel.localeselector
+def get_locale():
+    """determines the best match with our supported languages"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index():
+    """Returns index.html"""
+    return render_template('1-index.html')
+
+
+if __name__ == "__main__":
+    app.run(host="localhost", port=5000, debug=True)
