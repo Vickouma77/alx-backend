141c37d1dc142d49ffe792224d906c7bf8e1633c
vickouma77 <Vickprestone20@gmail.com>
2023-08-01 13:11:31 +0300

    0-app update

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -13,7 +13,7 @@ def index() -> str:
     """
     return index page
     """
-    return render_template('/0-index.html')
+    return render_template('0-index.html')
 
 
 if __name__ == "__main__":
