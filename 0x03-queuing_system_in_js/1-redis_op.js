48725bf3c1d6bb605207a9ec3251eb8a5f414000
vickouma77 <Vickprestone20@gmail.com>
2023-08-30 14:34:53 +0300

    1-redis_op

---
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -0,0 +1,31 @@
+#!/usr/bin/node
+
+import { createClient, print } from 'redis';
+
+const client = createClient();
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+  console.log(`Redis client not connected to the server: ${err}`);
+});
+
+function setNewSchool(schoolName, value) {
+  client.set(schoolName, value, print);
+}
+
+function displaySchoolValue(schoolName) {
+  client.get(schoolName, (error, result) => {
+    if (error) {
+      console.log(error);
+      throw error;
+    }
+    console.log(result);
+  });
+}
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
