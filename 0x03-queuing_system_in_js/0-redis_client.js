9ffc563d97d3bde5cf9864b7e0817d9ed23a2d68
vickouma77 <Vickprestone20@gmail.com>
2023-08-30 14:15:09 +0300

    0-redis_client

---
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+import { createClient } from 'redis';
+
+function redisConnect() {
+  const client = createClient();
+
+  client.on('connect', () => {
+    console.log('Redis client connected to the server');
+  }).on('error', (err) => {
+    console.log(`Redis client not connected to the server: ${err}`);
+  });
+}
+
+redisConnect();
