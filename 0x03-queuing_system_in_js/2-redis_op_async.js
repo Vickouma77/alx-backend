0003fb693496383f7453b4f6d7fd6886a46ebe58
vickouma77 <Vickprestone20@gmail.com>
2023-08-30 14:37:32 +0300

    2-redis_op_async

---
+++ 0x03-queuing_system_in_js/2-redis_op_async.js
@@ -0,0 +1,34 @@
+#!/usr/bin/node
+
+import { createClient, print } from 'redis';
+import { promisify } from 'util';
+
+const client = createClient();
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+  console.log(`Redis client not connected to the server: ${err}`);
+});
+
+function setNewSchool(schoolName, value) {
+  client.set(schoolName, value, print);
+}
+
+const get = promisify(client.get).bind(client);
+
+async function displaySchoolValue(schoolName) {
+  const result = await get(schoolName).catch((error) => {
+    if (error) {
+      console.log(error);
+      throw error;
+    }
+  });
+  console.log(result);
+}
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
